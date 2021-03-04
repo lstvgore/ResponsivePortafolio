b4a72615d86296688d8a456a7462d3270d921c3b
stvgo15 <stv.gore@gmail.com>
2021-03-04 05:45:31 -0800

    updates

---
+++ react-website-portafolio/src/components/Cards.js
@@ -0,0 +1,45 @@
+import { React } from 'react';
+import './Cards.css';
+import CardItem from './CardItem';
+
+function Cards(){
+    return (
+        <div className='cards'>
+            <h1>Projects</h1>
+            <div className='cards__container'>
+                <div className='cards__wrapper'>
+                    <ul className='cards__items'>
+                        <CardItem
+                        src='images/MurderMystery.gif'
+                        text='Murder Mystery'
+                        label='Gamers'
+                        path='/Portafolio'
+                        />
+                        <CardItem
+                        src='images/WeatherDash.gif'
+                        text='Weather Dashboard'
+                        label='APIS'
+                        path='/Portafolio'
+                        />
+                        </ul>
+                        <ul class='cards__items'>
+                        <CardItem
+                        src='images/NoteTaker.gif'
+                        text='NoteTaker'
+                        label='Node.js'
+                        path='/Portafolio'
+                        />
+                        <CardItem
+                        src='images/QuizPop.gif'
+                        text='QuizPop'
+                        label='Javascript'
+                        path='/Portafolio'
+                        />
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Cards;
---
+++ react-website-portafolio/src/components/Navbar.js
@@ -0,0 +1,63 @@
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css'
+import { Button } from './Button';
+
+function Navbar() {
+    const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
+
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
+    const showButton = () => {
+        if(window.innerWidth <= 960) {
+            setButton(false);
+        } else {
+            setButton(true);
+        }
+    };
+
+    window.addEventListener('resize', showButton);
+
+    return (
+       <>
+       <nav className="navbar">
+           <div className="navbar-container">
+            <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+                LSGORE <i className= 'fab.fa-typo3'/>  
+            </Link>
+            <div className='menu-icon' onClick={handleClick}>
+                <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+            </div>
+            <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                <li className='nav-item'>
+                    <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                        Home
+                    </Link>
+                </li>
+                <li className='nav-item'>
+                    <Link to='/Portafolio' className='nav-links' onClick={closeMobileMenu}>
+                        Portafolio
+                    </Link>
+                </li>
+                <li className='nav-item'>
+                    <Link to='/Contact' className='nav-links' onClick={closeMobileMenu}>
+                        Contact
+                    </Link>
+                </li>
+                <li className='nav-item'>
+                    <Link to='/About' className='nav-links-mobile' onClick={closeMobileMenu}>
+                        Sign Up
+                    </Link>
+                </li>
+            </ul>
+            {button && <Button buttonStyle='btn--outline'>Sign Up</Button>}
+           </div>
+       </nav>
+       </>
+    )
+}
+
+export default Navbar
+
